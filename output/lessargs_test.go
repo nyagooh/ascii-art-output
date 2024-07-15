d78752259f5db23e6f21dceb00368a05acdf298d
allkamau <allangithaiga5@gmail.com>
2024-07-15 15:16:45 +0300

    Add lessarg test

---
+++ output/lessargs_test.go
@@ -0,0 +1,14 @@
+package output
+
+import (
+	"testing"
+)
+
+func TestLessArguments(t *testing.T) {
+	// Test valid scenario
+	output := "output.txt" // Provide a unique output file name
+	input := "input string"
+	LessArguments(output, input)
+
+	// Add more test scenarios as needed to cover different cases
+}
